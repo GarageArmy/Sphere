450ccab811beccd4fa3999fd2c22759552a14c38
Gabriel025 <barnoth.g@gmail.com>
2016-05-16 20:33:34 +0200

    A fő State osztálya. (körcikkforgatás)

---
+++ core/src/com/mygdx/game/IngameState.java
@@ -0,0 +1,23 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Gabriel025 on 2016.05.16.
+ */
+public class IngameState extends State {
+
+    IngameState(StateManager sm) {
+        super(sm);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+
+    }
+}
