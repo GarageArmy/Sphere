1b6495a643e352d5ed27406e40880e07dacc1fc5
Gabriel025 <barnoth.g@gmail.com>
2016-05-24 21:52:31 +0200

    States now have a camera.

--- core/src/com/mygdx/game/State.java
+++ core/src/com/mygdx/game/State.java
@@ -1,6 +1,7 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
@@ -11,11 +12,15 @@ import com.badlogic.gdx.math.Vector3;
 public abstract class State {
 
     protected StateManager stateManager;
+    protected Camera camera;
 
-    public State (StateManager sm){
+    public State (StateManager sm) {
         stateManager = sm;
     }
 
-    public abstract void render (SpriteBatch batch);
+    public void render (SpriteBatch batch) {
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+    }
     public abstract void update ();
 }
