66336fda7fbb37061b266ca8bd58d22c9b9f224f
Gabriel025 <barnoth.g@gmail.com>
2016-05-24 21:54:21 +0200

    WIDTH and HEIGHT not needed.

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -8,8 +8,10 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Main extends ApplicationAdapter {
 
+	/*
 	public static final int HEIGHT = 800;
 	public static final int WIDTH = 480;
+	*/
 
 	SpriteBatch batch;
     StateManager statemgr;
@@ -25,7 +27,7 @@ public class Main extends ApplicationAdapter {
 	@Override
 	public void render () {
         statemgr.update();
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         statemgr.render(batch);
 	}
