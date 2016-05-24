11198495c9ce13bd6d68c3b426f4e03a4d6e0eab
Gabriel025 <barnoth.g@gmail.com>
2016-05-24 21:55:31 +0200

    IngameState development advances

--- core/src/com/mygdx/game/IngameState.java
+++ core/src/com/mygdx/game/IngameState.java
@@ -1,14 +1,44 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttribute;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 
 /**
  * Created by Gabriel025 on 2016.05.16.
  */
 public class IngameState extends State {
+    int numLayers; //Number of layers
+    int numSlices; //Number of slices per layer
+    int sliceSize; //Thickness of slices
+    int gapSize; //Size of gaps between layers and between slices
+    int sliceResolution; //Number of vertices per arc (2 arcs per slice)
+    Mesh circleSlices[]; //Meshes of the slices (one for every layer)
 
     IngameState(StateManager sm) {
         super(sm);
+        camera = new OrthographicCamera(800, 480);
+
+        numLayers = 6;
+        numSlices = 6;
+        sliceSize = 30;
+        gapSize = 10;
+        sliceResolution = 5;
+        circleSlices = new Mesh[numLayers];
+
+        float verts[] = new float[sliceResolution * 2];
+        float inds[] = new float[sliceResolution * 6 - 6];
+        for (int i = 0; i < numLayers; i++) {
+
+            circleSlices[i] = new Mesh(Mesh.VertexDataType.VertexBufferObject, true,
+                    sliceResolution * 2, sliceResolution * 6 - 6,
+                    VertexAttribute.Position(), VertexAttribute.ColorPacked());
+        }
+
+
     }
 
     @Override
@@ -18,6 +48,6 @@ public class IngameState extends State {
 
     @Override
     public void render(SpriteBatch batch) {
-
+        super.render(batch);
     }
 }
